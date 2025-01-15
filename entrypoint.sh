aaa3f9f0e40c7026e40f269a2b734c3cae6fde02
Lyndon Chivers <lyndychivs@gmail.com>
2025-01-15 19:53:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,16 @@ configFile=$1
 dashboardApiKey=$2
 version=$3
 
-if [ -n "$(echo $dashboardApiKey | grep -v '^[[:space:]]*$')" ]; then
-  echo "dashboard-api-key provided"
-  if [ -n "$(echo $version | grep -v '^[[:space:]]*$')" ]; then
-    echo "version provided: $version"
-    dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey --version $version
-  else
-    echo "version not provided"
-    dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey
-  fi
+if [ -z "${dashboardApiKey}" ]; then
+   echo "dashboard-api-key provided" 
+   if [ -z "${version}" ]; then
+      echo "version provided: $version"
+      dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey --version $version
+   else
+      echo "version not provided"
+      dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey
+   fi
 else
-  echo "dashboard-api-key not provided"
-  dotnet stryker --config-file $configFile
+   echo "dashboard-api-key not provided"
+   dotnet stryker --config-file $configFile
 fi
