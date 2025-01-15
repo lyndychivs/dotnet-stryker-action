bb9d68dd43cbeae28fafdfb4bd6d5b5e8bb361d8
Lyndon Chivers <lyndychivs@gmail.com>
2025-01-15 14:02:32 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ export PATH="$PATH:/root/.dotnet/tools"
 
 if [ -n "$(echo $2 | grep -v '^[[:space:]]*$')" ]; then
   echo "dashboard-api-key provided"
-  dotnet stryker --config-file $1 --dashboard-api-key $2
+  dotnet stryker --config-file $1 --dashboard-api-key $2 --version $3
 else
   echo "dashboard-api-key not provided"
-  dotnet stryker --config-file $1
+  dotnet stryker --config-file $1 --version $3
 fi
