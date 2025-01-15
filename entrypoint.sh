8802c6a6a3de8ab2b96bfe9b43da54c6f38448b4
Lyndon Chivers <lyndychivs@gmail.com>
2025-01-15 14:29:06 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,20 @@
 
 export PATH="$PATH:/root/.dotnet/tools"
 
-if [ -n "$(echo $2 | grep -v '^[[:space:]]*$')" ]; then
+configFile=$1
+dashboardApiKey=$2
+version=$3
+
+if [ -n "$(echo $dashboardApiKey | grep -v '^[[:space:]]*$')" ]; then
   echo "dashboard-api-key provided"
-  dotnet stryker --config-file $1 --dashboard-api-key $2 --version $3
+  if [ -n "$(echo $version | grep -v '^[[:space:]]*$')" ]; then
+    echo "version provided: $version"
+    dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey --version $version
+  else
+    echo "version not provided"
+    dotnet stryker --config-file $configFile --dashboard-api-key $dashboardApiKey
+  fi
 else
   echo "dashboard-api-key not provided"
-  dotnet stryker --config-file $1 --version $3
+  dotnet stryker --config-file $configFile
 fi
